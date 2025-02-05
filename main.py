0955625c2bf5c335f821ef3b821f353d6527866c
mayowa-kalejaiye <kalejaiyemayowa3@gmail.com>
2025-02-05 19:06:08 +0100

    Update main.py
    Modified Flask App to Bind to Assigned Port

--- main.py
+++ main.py
@@ -116,3 +116,7 @@ def classify_number():
 # Entry point
 # if __name__ == '__main__':
 #     app.run(debug=DEBUG_MODE)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=DEBUG_MODE)
+
